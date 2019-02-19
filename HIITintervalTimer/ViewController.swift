0cd45075bcc50452be5aea47ed6af16263c2fba4
Aca Teske <log192@gmail.com>
2019-02-19 18:59:08 +0100

    Initial Commit

---
+++ HIITintervalTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HIITintervalTimer
+//
+//  Created by Aleksandar Tesanovic on 2/19/19.
+//  Copyright © 2019 Aleksandar Tesanovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
